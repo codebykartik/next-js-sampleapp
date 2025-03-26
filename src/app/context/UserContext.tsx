f865bf5ce70b1674c43ac5e4e80df96c8db7aa17
codebykartik <sainikartik13579@gmail.com>
2025-03-26 18:25:44 +0530

    updated

--- src/app/context/UserContext.tsx
+++ src/app/context/UserContext.tsx
@@ -12,8 +12,8 @@ const UserContext = createContext<any>(null);
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const { data: session } = useSession();
   const [user, setUser] = useState({
-    firstName: "John",
-    lastName: "Doe",
+    firstName: "Kartik",
+    lastName: "Saini",
     photo: "/images/user/user-01.png",
     jobTitle: "Drug Researcher",
     userBio: "",
